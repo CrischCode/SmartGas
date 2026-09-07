b9be7d3207d0bdb77a33a0872dfe311d272c7795
CrischCode <cchamomorales@gmail.com>
2026-09-07 09:51:12 -0600

    feat: servicio de la API de Econnomy y service para calcular el viaje y sus gastos

---
+++ SmartGasApp/SmartGas.Api/Service/FuelEconomyApiService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using SmartGas.Api.DTOs;
+using SmartGas.Api.Interface;
+
+namespace SmartGas.Api.Service
+{
+    public class FuelEconomyApiService: IFuelEconomyApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FuelEconomyApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://www.fueleconomy.gov/ws/rest/");
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+        }
+
+        public async Task<IEnumerable<string>> GetMakesFromApiAsync()
+        {
+            try
+            {
+               var response = await _httpClient.GetAsync("vehicle/menu/make");
+               response.EnsureSuccessStatusCode();
+
+               var content = await response.Content.ReadAsByteArrayAsync();
+               var result = JsonSerializer.Deserialize<FuelEconomyMenuResponse>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+               return result?.MenuItems?.Select(m => m.Value) ?? Enumerable.Empty<string>();
+            }
+            catch
+            {
+                return Enumerable.Empty<string>();
+            }
+        }
+    }
+}
--- SmartGasApp/SmartGas.Api/Service/ViajeService.cs
+++ SmartGasApp/SmartGas.Api/Service/ViajeService.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SmartGas.Api.DTOs;
+using SmartGas.Api.Interface;
+using SmartGas.Api.Model;
 
 namespace SmartGas.Api.Service
 {
-    public class ViajeService
+    public class ViajeService: IViajeService
     {
+        public Task<TripResultDto> CalcularViajeAsync(TripRequestDto request)
+        {
+            if(request.DistanceKm <= 0 || request.KmPerGallon <= 0 || request.FuelPricePerGallon <= 0)
+            {
+                throw new ArgumentException("La distancia, el rendimiento y el precio del combustible deben ser mayores a cero.");
+            }
+
+            double gallonNeeded = request.DistanceKm / request.KmPerGallon;
+            double fuelCost = gallonNeeded = request.FuelPricePerGallon;
+            double totalTripCost = fuelCost + request.TollCost;
+
+            var result = new TripResultDto
+            {
+              DistanceKm = request .DistanceKm,
+              GallonsRequired = Math.Round(gallonNeeded,2),
+              FuelCostQ = Math.Round(totalTripCost, 2)  
+            };
+
+            return Task.FromResult(result);
+        }
+
         
     }
 }
